aa8296d6d8bb73af1d57587655879902117e608b
KUSHIRO13 <kushiroamigama@gmail.com>
2023-07-17 22:10:12 -0500

    Volver privada el metodo de clase generar_valor

--- POO/Leccion_06/Persona.py
+++ POO/Leccion_06/Persona.py
@@ -1,13 +1,17 @@
 class Persona:
     contador_persona = 0
     def __init__(self, nombre, edad):
-        Persona.contador_persona += 1
-        self.id_persona = Persona.contador_persona
+        self.id_persona = Persona.generar_valor()
         self._nombre = nombre
         self._edad = edad
 
     def __str__(self):
         return f"Persona: {self.id_persona} {self._nombre} {self._edad}"
+
+    @classmethod
+    def _generar_valor(cls):
+        cls.contador_persona += 1
+        return cls.contador_persona
 
     # Decoradores
     @property
@@ -31,10 +35,15 @@ class Persona:
 persona = Persona("Andres", 19)
 print(persona)
 
-persona15 = Persona("Karla", 25)
-print(persona15)
-
-persona2 = Persona("Eduardo", 43)
+persona2 = Persona("Karla", 25)
 print(persona2)
+
+persona3 = Persona("Eduardo", 43)
+print(persona3)
+
+# Persona.generar_valor()
+
+persona4 = Persona("Luis", 15)
+print(persona4)
 
 print(Persona.contador_persona)
