76d1f3fb6ebf39e5066ae02836f54db3c0a2b0a2
KUSHIRO13 <kushiroamigama@gmail.com>
2023-07-18 09:18:57 -0500

    Hacer compatible el metodo de clase contador productos Pd:Revisar porque no funciona el id producto de get

--- POO/Leccion_07/Producto.py
+++ POO/Leccion_07/Producto.py
@@ -1,22 +1,22 @@
 class Producto:
     contador_producto = 0
     def __init__(self, nombre, precio):
-        self._id_producto = Producto._contador_productos()
+        self.__id_producto = Producto._contador_productos()
         self._nombre = nombre
         self._precio = precio
 
     def __str__(self):
         return f"{Producto.separador(self)}\n" \
-               f"ID: {self._id_producto}\nNombre: {self._nombre}\nPrecio: {self._precio}" \
-               f"{Producto.separador(self)}"
+               f"ID: {self.__id_producto}\nNombre: {self._nombre}\nPrecio: {self._precio}" \
+               f"\n{Producto.separador(self)}"
 
     def separador(self):
-        return "=".center(50,"=")
+        return "=".center(20,"=")
 
     @classmethod
     def _contador_productos(cls):
-        Producto.contador_producto += 1
-        return Producto.contador_producto
+        cls.contador_producto += 1
+        return cls.contador_producto
 
     @property
     def nombre(self):
@@ -26,9 +26,9 @@ class Producto:
     def precio(self):
         return self._precio
 
-    @property
-    def _id_producto(self):
-        return self._id_producto
+    # @property
+    # def _id_producto(self):
+    #     return self._id_producto
 
     @nombre.setter
     def nombre(self, nombre):
@@ -38,12 +38,14 @@ class Producto:
     def precio(self, precio):
         self._precio = precio
 
-    @_id_producto.setter
-    def _id_producto(self, id_producto):
-        self._id_producto = id_producto
+    # @_id_producto.setter
+    # def _id_producto(self, id_producto):
+    #     self._id_producto = id_producto
 
 
 if __name__ == "__main__":
     producto1 = Producto("Manzana", 1500)
     print(producto1)
+    producto2 = Producto("Coca Cola", 3500)
+    print(producto2)
 
