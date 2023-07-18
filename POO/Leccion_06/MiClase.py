845ba9d9a38e2eacdb3943fb484f8e53b564c470
KUSHIRO13 <kushiroamigama@gmail.com>
2023-07-17 21:54:10 -0500

    Constantes

--- POO/Leccion_06/MiClase.py
+++ POO/Leccion_06/MiClase.py
@@ -3,6 +3,20 @@ class MiClase:
     def __init__(self, variable_instancia):
         self.variable_instancia = variable_instancia
 
+    def dinamico(self):
+        print("Metodo dinamico")
+        self.metodo_clase()
+        self.metodo_estatico()
+
+    @staticmethod
+    def metodo_estatico():
+        print(MiClase.variable_clase, " Metodo Estatico")
+
+    @classmethod
+    def metodo_clase(cls):
+        print(cls.variable_clase, " Metodo de Clase")
+
+"""
 print(MiClase.variable_clase)
 
 miclase = MiClase("Hola")
@@ -15,3 +29,14 @@ print(miclase.variable_instancia)
 # Al vuelo
 MiClase.variable_clase2 = "Variable dos"
 print(miclase.variable_clase2)
+miclase.metodo_estatico()
+MiClase.metodo_estatico()
+
+# Metodo de clase
+miclase.metodo_clase()
+MiClase.metodo_clase()
+"""
+
+miObjecto1 = MiClase("Variable")
+miObjecto1.metodo_clase()
+miObjecto1.dinamico()
---
+++ POO/Leccion_06/constantes.py
@@ -0,0 +1,4 @@
+class Matematicas:
+    PI = 3.1416
+
+MI_CONSTANTE = "Valor de constante"
